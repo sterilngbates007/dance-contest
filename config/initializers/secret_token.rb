a57b164048b124c1fe247d9aaa6db60bbb584626
Sterling Bates <sterling@stepresearch.com>
2012-06-16 14:09:04 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DanceContest::Application.config.secret_token = 'd1aa374c08cd787788d057364429b8bc1a7ce2ccea893f42c5a2d06d6b2b61f51fe22ae1108df8888615961c4dfac464ab086e71ae9bfd2dcfbe960ed51ef342'
